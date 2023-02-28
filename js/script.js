51bb7bdd417150c2937eb21e3ac65fbb783a2459
yesNemo <chalenko811@gmail.com>
2023-02-28 16:59:53 +0100

    fir

---
+++ js/script.js
@@ -0,0 +1,20 @@
+//Перший вид коментувванняasdasd
+
+/*
+Другий вид коментуванняasdads
+ */
+let Name = prompt("Як твоє ім'я");
+alert('Hello '+ Name);
+let Age = prompt("Рік народження?");
+alert(2023 - Age);
+let Perimeter = prompt("Довжина сторони квадрату, а я скажу периметр");
+alert(Perimeter * 4);
+let Radius = prompt("Радіус кола")
+alert(Radius * Radius * Math.PI);
+let Distansion = prompt("Відстань в кілометрах між двома містами");
+let Time = prompt("За скільки годин ти хочеш дістатися")
+let Speed = (Distansion / Time)
+alert('Швидкість з якою потрібно рухатьсь буде ' +  Speed + 'км/ч');
+let ConvertorInEuro = prompt("Введіть валюту в долларах");
+let Euro = ConvertorInEuro * 0.94;
+alert(ConvertorInEuro + 'USD в EURO = '+ Euro);
